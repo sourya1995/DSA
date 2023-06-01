5c01cc6e664ac79cc5d3d88aa1bf9a3be5615fe7
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-01 09:33:23 +0530

    Linked List - insert at End

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -27,6 +27,24 @@ public class SinglyLinkedList<T> {
         size++; //increment the size of the list
     }
 
+    public void insertAtEnd(T data) {
+        if (isEmpty()) {
+            insertAtHead(data);
+            return; 
+        }
+
+        Node newNode = new Node(); //create a new node
+        newNode.data = data; //assign data
+        newNode.nextNode = null;
+
+        Node last = headNode; //iterate to last element of the list
+        while (last.nextNode != null){
+            last = last.nextNode;
+        }
+        last.nextNode = newNode; //point the next of last, to the new node
+        size++;
+    }
+
     public void printList() {
         if(isEmpty()){
             System.out.println("List is empty");
