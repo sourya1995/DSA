ff26e7c41fa02dc7630d2e237de90e36d6243336
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-02 12:06:33 +0530

    Problem: reverse K elements of a queue

--- Queues/QueueDemo.java
+++ Queues/QueueDemo.java
@@ -1,4 +1,5 @@
 package Queues;
+import Stacks.Stack;
 
 public class QueueDemo {
 
@@ -16,6 +17,27 @@ public class QueueDemo {
             queue.enqueue(s2);
         }
         return result;
+    }
+
+    public static<V> void reverseK(Queue<V> queue, int k){
+        if(queue.isEmpty() || k <=0){
+            return;
+        }
+        Stack<V> stack = new Stack<>(k);
+        while(!stack.isFull()){
+            stack.push(queue.dequeue());
+        }
+
+        while(!stack.isEmpty()){
+            queue.enqueue(stack.pop());
+        }
+
+        int size = queue.getCurrentSize();
+        for(int i = 0; i < size - k; i++){
+            queue.enqueue(queue.dequeue());
+        }
+
+
     }
     public static void main(String[] args){
 
--- Stacks/Stack.java
+++ Stacks/Stack.java
@@ -4,12 +4,18 @@ public class Stack<V> {
     private int maxSize;
     private int top;
     private V array[];
+    private int currentSize;
 
     @SuppressWarnings("unchecked")
     public Stack(int max_size) {
         this.maxSize = max_size;
         this.top = -1;
         array = (V[]) new Object[max_size]; // generic arrays are not supported
+        this.currentSize = 0;
+    }
+
+    public int getCurrentSize() {
+        return currentSize;
     }
 
     public int getCapacity() {
@@ -37,12 +43,14 @@ public class Stack<V> {
             return;
         }
         array[++top] = value;
+        currentSize++;
     }
 
     public V pop(V value) {
         if(isEmpty()){
             return null;
         }
+        currentSize--;
         return array[top--];
     }
 }
