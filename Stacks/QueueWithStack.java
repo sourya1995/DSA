c28cc3d00fb252a1a3c64df27cf5f765ac92fc3d
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-02 12:27:28 +0530

    Problem: Implement Queue using Stacks

---
+++ Stacks/QueueWithStack.java
@@ -0,0 +1,31 @@
+package Stacks;
+
+public class QueueWithStack <V>{
+    Stack<V> stack1;
+    Stack<V> stack2;
+    
+    public QueueWithStack(int maxSize){
+        stack1 = new Stack<>(maxSize);
+        stack2 = new Stack<>(maxSize);
+    }
+
+    public boolean isEmpty(){
+        return stack1.isEmpty();
+    }
+
+    public void enqueue(V v){
+        stack1.push(v);
+    }
+
+    public V dequeue(){
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+
+        V result = stack2.pop();
+
+        while(!stack2.isEmpty()){
+            stack1.push(stack2.pop());
+        }
+    }
+}
