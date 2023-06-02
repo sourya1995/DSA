c877f8f3d356a5004f3cbab77a8eb07264b6604c
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-02 12:31:21 +0530

    optimizing the solution

--- Stacks/QueueWithStack.java
+++ Stacks/QueueWithStack.java
@@ -1,33 +1,35 @@
 package Stacks;
 
-public class QueueWithStack <V>{
+public class QueueWithStack<V> {
     Stack<V> stack1;
     Stack<V> stack2;
-    
-    public QueueWithStack(int maxSize){
+
+    public QueueWithStack(int maxSize) {
         stack1 = new Stack<>(maxSize);
         stack2 = new Stack<>(maxSize);
     }
 
-    public boolean isEmpty(){
-        return stack1.isEmpty();
+    public boolean isEmpty() {
+        return (stack1.isEmpty() && stack2.isEmpty());
     }
 
-    public void enqueue(V v){
+    public void enqueue(V v) {
         stack1.push(v);
     }
 
-    public V dequeue(){
-        while(!stack1.isEmpty()){
-            stack2.push(stack1.pop());
+    public V dequeue() {
+
+        if (isEmpty()) {
+            return null;
+        } else if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+
+            return stack2.pop();
+        } else {
+            return stack2.pop();
         }
 
-        V result = stack2.pop();
-
-        while(!stack2.isEmpty()){
-            stack1.push(stack2.pop());
-        }
-
-        return result;
     }
 }
