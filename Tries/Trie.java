73a8c3ee9ffab1a49dacdefc96e7f574c4834bbb
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-04 20:11:31 +0530

    Problem: Word Formation using Trie

--- Tries/Trie.java
+++ Tries/Trie.java
@@ -154,5 +154,27 @@ public class Trie {
     private TrieNode getRoot() {
         return this.root;
     }
+
+    public static boolean isFormationPossible(String[] dict, String word){
+        if(word.length() < 2 || dict.length < 2){
+            return false;
+        }
+
+        Trie trie = new Trie();
+
+        for(int i=0; i < dict.length; i++){
+            trie.insert(dict[i]);
+        }
+
+        for(int i = 0; i < word.length(); i++){
+            String first = word.substring(0, i);
+            String second = word.substring(i, word.length());
+            if(trie.search(first) && trie.search(second)){
+                return true;
+            }
+        }
+
+        return false;
+    }
   
 }
