d6734333e37af82e1d76540f948a64732f49f6de
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-04 18:49:07 +0530

    deleting in a trie

--- Tries/Trie.java
+++ Tries/Trie.java
@@ -27,7 +27,77 @@ public class Trie {
         }
         currentNode.markAsLeaf();
     }
-    public boolean search(String key) {return false;}
-    public boolean delete(String key) {return false;}
-    
+    public boolean search(String key) {
+        if(key == null) return false;
+        key = key.toLowerCase();
+        TrieNode currentNode = this.root;
+        int index = 0;
+
+        for(int level = 0; level < key.length(); level++){
+            index = getIndex(key.charAt(level));
+            if(currentNode.children[index] == null){
+                return false;
+            }
+            currentNode = currentNode.children[index];
+        }
+
+        return currentNode.isEndWord;
+    }
+
+    private boolean hasNoChildren(TrieNode currentNode){
+        for(int i=0; i < currentNode.children.length; i++){
+            if((currentNode.children[i] != null))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean deleteHelper(String key, TrieNode currentNode, int length, int level){
+        boolean deletedSelf = false;
+        if(currentNode == null){
+            System.out.println("key does not exist");
+            return deletedSelf;
+        }
+
+        if(level == length){
+            if(hasNoChildren(currentNode)){
+                currentNode = null;
+                deletedSelf = true;
+            }else{
+                currentNode.unmarkAsLeaf();
+                deletedSelf = false;
+            }
+        }
+        else {
+            TrieNode childNode = currentNode.children[getIndex(key.charAt(level))];
+            boolean childDeleted = deleteHelper(key, childNode, length, level + 1);
+            if(childDeleted){
+                currentNode.children[getIndex(key.charAt(level))] = null;
+                if(currentNode.isEndWord){
+                    deletedSelf = false;
+                }
+                else if(!hasNoChildren(currentNode)){
+                    deletedSelf = false;
+                }
+                else{
+                    currentNode = null;
+                    deletedSelf = true;
+                }
+            }
+            else{
+                deletedSelf = false;
+            }
+        }
+        return deletedSelf;
+    }
+
+    public void delete(String key){
+        if((root == null) || (key == null)){
+            System.out.println("Null key or empty trie error");
+            return;
+        }
+        deleteHelper(key, root, key.length(), 0);
+    }
+
+  
 }
