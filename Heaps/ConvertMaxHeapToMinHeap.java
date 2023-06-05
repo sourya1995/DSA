ce6c2dce0ad2befdc0224a43085e5813e7623b4c
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-05 09:06:48 +0530

    Problem: find K smallest elements in an array, ie, min priority queue using an array

--- Heaps/ConvertMaxHeapToMinHeap.java
+++ Heaps/ConvertMaxHeapToMinHeap.java
@@ -2,6 +2,27 @@ package Heaps;
 
 public class ConvertMaxHeapToMinHeap {
 
+    public static int[] findKSmallest(int[] arr, int k){
+        int arraySize = arr.length;
+        if(k <= arraySize){
+            int[] result = new int[k];
+            for(int i=0; i < k; i++){
+                result[i] = removeMin(arr, arraySize);
+                --arraySize;
+            }
+            return result;
+        }
+        System.out.println("Value of k = "+k + "is out of bounds");
+        return arr;
+    }
+
+    public static int removeMin(int[] heapArray, int heapSize){
+        buildMinHeap(heapArray, heapSize);
+        int min = heapArray[0];
+        heapArray[0] = heapArray[heapSize - 1];
+        return min;
+    }
+
     public static void convertMax(int[] maxHeap){
         buildMinHeap(maxHeap, maxHeap.length);
     }
@@ -17,6 +38,7 @@ public class ConvertMaxHeapToMinHeap {
         while(smallest < heapSize/2){
             int left = 2 * index + 1;
             int right = 2 * index + 2;
+
             if(left < heapSize && heapArray[left] < heapArray[index]){
                 smallest = left;
             }
@@ -30,7 +52,7 @@ public class ConvertMaxHeapToMinHeap {
                 heapArray[index] = heapArray[smallest];
                 heapArray[smallest] = temp;
                 index = smallest;
-            } else{
+            } else {
                 break;
             }
         }
