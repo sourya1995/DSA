ecea963643c11fd8fba81f61a2ee0f0e5b93d715
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-05 08:23:31 +0530

    adding comments for clarity

--- Heaps/MaxHeap.java
+++ Heaps/MaxHeap.java
@@ -1,16 +1,20 @@
 package Heaps;
 
 public class MaxHeap {
+    /*index - current position
+     * heapSize - how many elements
+     * largest - pointer/current element
+     */
     private void maxHeapify(int[] heapArray, int index, int heapSize){
         int largest = index; //index here is k
         while(largest < heapSize / 2){
             int left = 2 * index + 1;
             int right = 2 * index + 2;
             if(left < heapSize && heapArray[left] > heapArray[index]){
-                largest = left;
+                largest = left; //move pointer
             }
             if(right < heapSize && heapArray[right] > heapArray[largest]){
-                largest = right;
+                largest = right; 
             }
 
             if(largest != index){
