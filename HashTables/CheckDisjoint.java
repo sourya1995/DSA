6b83ef749e5fdc1bfc4a53b239493c292e01cec5
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-05 12:16:05 +0530

    Problem: check if 2 arrays are disjoint

---
+++ HashTables/CheckDisjoint.java
@@ -0,0 +1,24 @@
+package HashTables;
+
+import java.util.HashSet;
+
+public class CheckDisjoint {
+    public static boolean isDisjoint(int[] arr1, int[] arr2){
+        HashSet<Integer> hset = new HashSet<>();
+        for(int i=0; i<arr1.length; i++){
+            if(!hset.contains(arr1[i])){
+                hset.add(arr1[i]);
+            }
+
+        }
+
+        for(int i =0; i<arr2.length; i++){
+            if(hset.contains(arr2[i])){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+    
+}
