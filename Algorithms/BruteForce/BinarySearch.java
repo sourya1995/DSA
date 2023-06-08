3ed7600bbc92fba2b9313bdefadbed777dd0f403
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-08 16:15:07 +0530

    Binary Search

---
+++ Algorithms/BruteForce/BinarySearch.java
@@ -0,0 +1,35 @@
+package Algorithms.BruteForce;
+
+public class BinarySearch {
+    static int binarySearch(int s, int arr[], int arrSize) {
+        if (arrSize <= 0) {
+            return -1;
+        }
+
+        if (arrSize == 1) {
+            if (arr[0] == s) {
+                return 0;
+            }
+            return -1;
+        }
+
+        int start = 0;
+        int end = arrSize - 1;
+        int mid;
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (arr[mid] < s) {
+                start = mid + 1;
+            } else if (arr[mid] > s) {
+                end = mid - 1;
+            } else {
+                return mid;
+
+            }
+
+        }
+        return -1;
+    }
+
+}
