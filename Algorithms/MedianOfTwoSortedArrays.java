db7e59a7d166fb89e2116989dbc266fa4e57f562
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-08 20:55:38 +0530

    adding log(n) solution

--- Algorithms/MedianOfTwoSortedArrays.java
+++ Algorithms/MedianOfTwoSortedArrays.java
@@ -4,6 +4,7 @@ import Algorithms.Sorting.Helper;
 
 public class MedianOfTwoSortedArrays {
     static Helper helper = new Helper();
+
     public static double getMedianOfTwoSortedArrays(int[] array1, int[] array2) {
         int size1 = array1.length;
         int size2 = array2.length;
@@ -39,6 +40,58 @@ public class MedianOfTwoSortedArrays {
             return median;
         } else {
             return (median + prevMedian) / 2.0;
+        }
+
+    }
+
+    public static double getMedianOfTwoSortedArraysInLog(int[] array1, int[] array2) {
+        int size1 = array1.length;
+        int size2 = array2.length;
+        int end_index = size1;
+        int start_index = 0;
+        int i = 0;
+        int j = 0;
+        int median = 0;
+
+        while (start_index <= end_index) {
+            i = (start_index + end_index) / 2;
+            j = ((size1 + size2 + 1) / 2) - i;
+
+            if (i < size1 && j > 0 && array2[j - 1] > array1[i]) {
+                start_index = i + 1;
+            }
+
+            else if (i > 0 && j < size2 && array2[j] < array1[i - 1]) {
+                end_index = i - 1;
+            }
+
+            else {
+                if (i == 0) {
+                    median = array2[i - 1];
+                }
+
+                else if (j == 0) {
+                    median = array1[i - 1];
+                }
+
+                else {
+                    median = helper.max_index(array1[i - 1], array2[j - 1]);
+                    break;
+                }
+
+                if ((size1 + size2) % 2 == 1)
+                    return (double) median;
+
+                if (i == size1)
+                    return (median + array2[j]) / 2.0;
+
+                if (j == size2)
+                    return (median + array1[i]) / 2.0;
+
+                return (median + helper.minimum(array1[i],
+                        array2[j])) / 2.0;
+            }
+
         }
 
     }
