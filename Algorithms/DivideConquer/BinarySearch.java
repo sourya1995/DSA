b66e4ae680b7dc1941c974718dbe3480f17da7b6
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-16 14:07:19 +0530

    Divide and Conquer: Binary Search

---
+++ Algorithms/DivideConquer/BinarySearch.java
@@ -0,0 +1,16 @@
+package Algorithms.DivideConquer;
+
+public class BinarySearch {
+    public static int binarySearch(int arr[], int left, int right, int key){
+        if(right >= left){
+            int mid = left + (right - left)/2;
+            if(arr[mid] == key){
+                return mid;
+            }else if(arr[mid] > key){
+                return binarySearch(arr, left, mid-1, key);
+            }else{
+                return binarySearch(arr, mid+1, right, key);
+            }
+        }
+    }
+}
