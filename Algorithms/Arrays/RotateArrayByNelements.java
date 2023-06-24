4a664f79aa346b2c5c79a7296c841e7cbca70880
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-24 10:34:07 +0530

    Rotate Array by N elements

--- Algorithms/Arrays/RotateArrayByNelements.java
+++ Algorithms/Arrays/RotateArrayByNelements.java
@@ -1,5 +1,6 @@
 package Algorithms.Arrays;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RotateArrayByNelements {
@@ -23,5 +24,27 @@ public class RotateArrayByNelements {
         ReverseArray(nums, 0, len - 1);
 		ReverseArray(nums, 0, n - 1);
 		ReverseArray(nums, n, len - 1);
+    }
+
+    static void RotateArrayByN(List<Integer> nums, int n){
+        int len = nums.size();
+        n = n %len;
+        if(n < 0) {
+            n = n + len;
+        }
+
+        List<Integer> temp = new ArrayList<Integer>();
+
+        for (int i = len - n; i < len; i++) {
+            temp.add(nums.get(i))
+        }
+
+        for (int i = len - 1; i>=n; i--){
+            nums.set(i, nums.get(i-n));
+        }
+
+        for(int i = 0; i < n; i++){
+            nums.set(i, temp.get(i));
+        }
     }
 }
