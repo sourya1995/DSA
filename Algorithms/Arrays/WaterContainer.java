ff25491b4bfb0e44461bfa043fd7dff7e8f0703f
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-29 18:04:01 +0530

    Find Max Water Container area

---
+++ Algorithms/Arrays/WaterContainer.java
@@ -0,0 +1,16 @@
+package Algorithms.Arrays;
+
+public class WaterContainer {
+    public static int maxWaterAreaContainer(int[] height) {
+        int maxArea = 0, left = 0, right = height.length - 1;
+        while (left < right) {
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
