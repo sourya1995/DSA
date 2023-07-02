d05457b0484199995a8a210b152df532a8dc56e3
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-02 15:08:58 +0530

    Verify Alien Dictionary

---
+++ Algorithms/Strings/VerifyAlienDictionary.java
@@ -0,0 +1,33 @@
+package Algorithms.Strings;
+
+import java.util.Map;
+
+public class VerifyAlienDictionary {
+    public static boolean verifyAlienDictionary(String[] words, String order){
+        if(words.length) == 1 return true;
+
+        Map<Character, Integer> keyMap = new HashMap<>();
+
+        for(int i = 0; i < order.length(); i++){
+            keyMap.put(order.charAt(i), i);
+        }
+
+        for(int i = 0; i < words.length; i++){
+            for(int j = 0; j < words[i].length(); j++){
+                if(j >= words[i+1].length()){
+                    return false;
+                }
+
+                if(words[i].charAt(j) != words[i+1].charAt(j)){
+                    if(keyMap.get(words[i].charAt(j)) > keyMap.get(words[i+1].charAt(j))){
+                        return false;
+                    } else {
+                        break;
+                    }
+        }
+    }
+}
+
+return true;
+    }
+}
