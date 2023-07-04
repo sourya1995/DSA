5da2794be81a74ecaa67d52f210403c50fd43388
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-05 03:52:08 +0530

    Kadane's Algorithm

---
+++ Algorithms/DynamicProgramming/MaxSumSubarray.java
@@ -0,0 +1,25 @@
+package Algorithms.DynamicProgramming;
+
+public class MaxSumSubarray {
+    static int findMaxSumSubarray(int[] nums){
+        if(nums.length < 1) return 0;
+
+        int currSubarrMax = nums[0];
+        int globalMax = nums[0];
+
+        for (int i = 0; i < nums.length; i++){
+            if(currSubarrMax < 0){
+                currSubarrMax = nums[i];
+            }
+
+            else {
+                currSubarrMax = currSubarrMax + nums[i];
+            }
+
+            if(currSubarrMax > globalMax){
+                globalMax = currSubarrMax;
+            }
+        }
+        return globalMax;
+    }
+}
